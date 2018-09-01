92a04a55c90c9988ac0dfc17b046f64c238694d1
Bhavya Kamboj <bhavyakamboj@live.com>
2018-09-01 16:47:10 +0530

    updated

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -17,13 +17,9 @@ class Welcome extends Component {
 
     return (
       <Container>
-        <Jumbotron>
-          <h1 className="display-3">Hare Krishna</h1>
-          <p className="lead">Welcome to Vedic Quiz by Iskcon Dwarka</p>
-          <p className="lead">
-          <Link to="/quiz" color="primary" className="btn btn-primary">Start Quiz</Link>
+        <p className="lead">
+          <Link to="/quiz" color="primary" className="btn btn-primary" id="startQuiz">Start Quiz</Link>
         </p>
-        </Jumbotron>
       </Container>
     );
   }
